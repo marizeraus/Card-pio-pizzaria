7d872d6eeda41c32913883e7371e19c7149831b5
Mariana Suarez <38335557+marizeraus@users.noreply.github.com>
2019-06-29 23:02:36 -0300

    10/20, ta saindo

--- cardapio_pizza/arvore_b_mais.c
+++ cardapio_pizza/arvore_b_mais.c
@@ -110,99 +110,106 @@ int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo
     fseek(dados_file, folha_ptr, SEEK_SET);
     TNoFolha* folha_node = le_no_folha(d, dados_file);
 
-    //pode inserir na folha sem problemas
-    if(folha_node->m < 2 * d){
-        //inserir nova folha na posicao correta e dar shift right no resto{
-        folha_node->pizzas[folha_node->m] = nova_pizza;
-        folha_node->m += 1;
-        sort_no(folha_node);
+    int taNaArvore = 0;
+    for (int i=0; i<folha_node->m; i++){
+        if (folha_node->pizzas[i]->cod==cod) taNaArvore=1;
+    }
+    if (taNaArvore) return -1;
+    else {
+        //pode inserir na folha sem problemas
+        if (folha_node->m < 2 * d) {
+            //inserir nova folha na posicao correta e dar shift right no resto{
+            folha_node->pizzas[folha_node->m] = nova_pizza;
+            folha_node->m += 1;
+            sort_no(folha_node);
 
-        //sobrescreve a folha
+            //sobrescreve a folha
+            fseek(dados_file, folha_ptr, SEEK_SET);
+            salva_no_folha(d, folha_node, dados_file);
+            fclose(dados_file);
+            return folha_ptr;
+        }
+        //caso folha esteja cheia, particionar
+        /*
+        //nova folha terão d+1 dados
+        int new_size = d+1;
+        TNoFolha* nova_folha_node = cria_no_folha(d, folha_node->pont_pai, folha_node->pont_prox, new_size, nova_pizza);
+
+        //separar os dados entre as 2 folhas(fazer uma c com (2*d + 1) pizzas,
+        // adicionar todos os da folha e mais o novo, ordenar por codigo e separar entre as 2 folhas)
+
+        //pegar a chave que vai subir para o no interno
+        int chave_nova_pizza = nova_folha_node->pizzas[0]->cod;
+
+        //ir ate o final do arquivo de dados para pegar o novo ponteiro
+        fseek(dados_file, 0, SEEK_END);
+        int new_ptr = ftell(dados_file);
+
+        //salva a nova folha
+        salva_no_folha(d, nova_folha_node, dados_file);
+        //sobrescreve a folha antiga
         fseek(dados_file, folha_ptr, SEEK_SET);
         salva_no_folha(d, folha_node, dados_file);
-        fclose(dados_file);
-        return folha_ptr;
+
+        //agora é preciso alterar os arquivos de indice
+        //caso a raiz fosse folha, o arquivo de indices estarão vazio
+        if(meta->raiz_folha){
+            //1 indice apenas
+            int m = 1;
+            //pont_pai = -1 ja q ele vai ser a raiz
+            int pont_pai = -1;
+            int aponta_folha = 1;
+            TNoInterno* in_node = no_interno(d, m, pont_pai, aponta_folha);
+            in_node->p[0] = folha_ptr;
+            in_node->p[1] = new_ptr;
+            in_node->chaves[0] = chave_nova_pizza;
+
+            //escrever no final do arquivo e pegar o ponteiro
+            fseek(indice_file, 0, SEEK_END);
+            int root_ptr = ftell(indice_file);
+            salva_no_interno(d, in_node, indice_file);
+            meta->raiz_folha = 0;
+            meta->pont_raiz = root_ptr;
+
+            //reescrever metadados
+            fseek(metadados_file, 0, SEEK_SET);
+            salva_metadados(meta, metadados_file);
+        }
+
+        fseek(indice_file, folha_node->pont_pai, SEEK_SET);
+        TNoInterno* in_node = le_no_interno(d, indice_file);
+
+        int i = 0;
+
+        //enquanto os nos internos estiverem cheios, particionar eles
+        while(in_node->m == 2*d){
+            //Qntd de elementos da nova pagina
+            //Achando o pai para particionar
+            int pai_ptr = busca(in_node->pont_pai,nome_arquivo_metadados,nome_arquivo_indice,nome_arquivo_dados,d);
+            fseek(indice_file, folha_ptr * tamanho_no_interno(d), SEEK_SET);
+            TNoInterno* new_node = le_no_interno(d,indice_file);
+
+            //Cria um novo Nó
+            TNoInterno *new_apontado = cria_no_interno(d,in_node->m,in_node->pont_pai,in_node->aponta_folha,in_node->m/2);
+
+            //"Transferencia de dados da pagina antiga para a nova"
+            new_node->aponta_folha = busca(new_apontado->chaves[2*d - i],nome_arquivo_metadados,nome_arquivo_indice,nome_arquivo_dados,d);
+            new_apontado->p[i] = in_node->p[2*d - 1];
+            i++;
+
+            //Danndo free nos nodes criados para a transferencia
+            libera_no_interno(new_node);
+            libera_no_interno(new_apontado);
+
+            //Atualizando o arquivo
+            salva_no_interno(d,new_node,indice_file);
+        }
+        int final_ptr = in_node->p[i];
+        libera_no_interno(in_node);
+        fclose(indice_file);
+        return final_ptr;
+        */
     }
-    //caso folha esteja cheia, particionar
-    /*
-    //nova folha terão d+1 dados
-    int new_size = d+1;
-    TNoFolha* nova_folha_node = cria_no_folha(d, folha_node->pont_pai, folha_node->pont_prox, new_size, nova_pizza);
-
-    //separar os dados entre as 2 folhas(fazer uma c com (2*d + 1) pizzas,
-    // adicionar todos os da folha e mais o novo, ordenar por codigo e separar entre as 2 folhas)
-
-    //pegar a chave que vai subir para o no interno
-    int chave_nova_pizza = nova_folha_node->pizzas[0]->cod;
-
-    //ir ate o final do arquivo de dados para pegar o novo ponteiro
-    fseek(dados_file, 0, SEEK_END);
-    int new_ptr = ftell(dados_file);
-
-    //salva a nova folha
-    salva_no_folha(d, nova_folha_node, dados_file);
-    //sobrescreve a folha antiga
-    fseek(dados_file, folha_ptr, SEEK_SET);
-    salva_no_folha(d, folha_node, dados_file);
-
-    //agora é preciso alterar os arquivos de indice
-    //caso a raiz fosse folha, o arquivo de indices estarão vazio
-    if(meta->raiz_folha){
-        //1 indice apenas
-        int m = 1;
-        //pont_pai = -1 ja q ele vai ser a raiz
-        int pont_pai = -1;
-        int aponta_folha = 1;
-        TNoInterno* in_node = no_interno(d, m, pont_pai, aponta_folha);
-        in_node->p[0] = folha_ptr;
-        in_node->p[1] = new_ptr;
-        in_node->chaves[0] = chave_nova_pizza;
-
-        //escrever no final do arquivo e pegar o ponteiro
-        fseek(indice_file, 0, SEEK_END);
-        int root_ptr = ftell(indice_file);
-        salva_no_interno(d, in_node, indice_file);
-        meta->raiz_folha = 0;
-        meta->pont_raiz = root_ptr;
-
-        //reescrever metadados
-        fseek(metadados_file, 0, SEEK_SET);
-        salva_metadados(meta, metadados_file);
-    }
-
-    fseek(indice_file, folha_node->pont_pai, SEEK_SET);
-    TNoInterno* in_node = le_no_interno(d, indice_file);
-
-    int i = 0;
-
-    //enquanto os nos internos estiverem cheios, particionar eles
-    while(in_node->m == 2*d){
-        //Qntd de elementos da nova pagina
-        //Achando o pai para particionar
-        int pai_ptr = busca(in_node->pont_pai,nome_arquivo_metadados,nome_arquivo_indice,nome_arquivo_dados,d);
-        fseek(indice_file, folha_ptr * tamanho_no_interno(d), SEEK_SET);
-        TNoInterno* new_node = le_no_interno(d,indice_file);
-
-        //Cria um novo Nó
-        TNoInterno *new_apontado = cria_no_interno(d,in_node->m,in_node->pont_pai,in_node->aponta_folha,in_node->m/2);
-
-        //"Transferencia de dados da pagina antiga para a nova"
-        new_node->aponta_folha = busca(new_apontado->chaves[2*d - i],nome_arquivo_metadados,nome_arquivo_indice,nome_arquivo_dados,d);
-        new_apontado->p[i] = in_node->p[2*d - 1];
-        i++;
-
-        //Danndo free nos nodes criados para a transferencia
-        libera_no_interno(new_node);
-        libera_no_interno(new_apontado);
-
-        //Atualizando o arquivo
-        salva_no_interno(d,new_node,indice_file);
-    }
-    int final_ptr = in_node->p[i];
-    libera_no_interno(in_node);
-    fclose(indice_file);
-    return final_ptr;
-    */
 }
 
 int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
